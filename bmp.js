be725825bc6cced479699263d3bd676f42cceece
EnkeyCZ <enkeymc@gmail.com>
2016-01-31 17:05:00 +0100

    finishing touches

--- bmp.js
+++ bmp.js
@@ -1,21 +1,30 @@
+/*
+
+  Author: Martin Omacht
+  Description: Class for loading, saving, transforming and rendering BMP files
+
+ */
+
+
 // enum: vertical, horizontal
 var Direction = {
 	HORIZONTAL: 0,
 	VERTICAL: 1
-}
+};
 
-// loads bitmap and stores it
-function BMP (buffer) {
+// Constructor that loads bitmap from buffer and stores it
+function BMP(buffer) {
 	this.imageData = null;
 	this.bitmap = {};
 
-	this.storeBitmap(buffer);  
+	this.storeBitmap(buffer);
 	this.storeImageData();
 }
 
 BMP.prototype = {
   	constructor: BMP,
 
+    // Rotates bitmap by 90 degrees
   	// ccw (counter clockwise) = true, false
   	rotate:function (ccw) {
       var byteCount = Math.ceil(this.bitmap.infoheader.biBitCount / 8);
@@ -47,14 +56,20 @@ BMP.prototype = {
         }
       }
 
+      // swap dimensions
       this.bitmap.infoheader.biWidth = height;
       this.bitmap.infoheader.biHeight = width;
+      // store new calculated stride
       this.bitmap.stride = newStride;
+      // store rotated pixels
       this.bitmap.pixels = rotated;
 
+      // generate new ImageData for canvas rendering
       this.storeImageData();
   	},
 
+    // Flips bitmap verticaly or horizontaly
+    // see: enum Direction (line 10)
   	flip:function (direction) {
       var height = this.bitmap.infoheader.biHeight;
       var byteCount = Math.ceil(this.bitmap.infoheader.biBitCount / 8);
@@ -89,13 +104,47 @@ BMP.prototype = {
           }
         }
   		}
+
+      // generate new ImageData for canvas rendering
   		this.storeImageData();
   	},
 
-  	save:function (filename) {
 
+    // saves bitmap to file
+    // currently working only for 24bit bitmaps
+  	save:function (filename) {
+      // allocate buffer for header information
+      var headerBuffer = new ArrayBuffer(54);
+      // create dataview for buffer
+      datav = new DataView(headerBuffer);
+      
+      // write file header
+	    datav.setUint16(0, this.bitmap.fileheader.bfType, true);
+	    datav.setUint32(2, this.bitmap.fileheader.bfSize, true);
+	    datav.setUint16(6, this.bitmap.fileheader.bfReserved1, true);
+	    datav.setUint16(8, this.bitmap.fileheader.bfReserved2, true);
+      datav.setUint32(10, this.bitmap.fileheader.bfOffBits, true);
+
+      // write info header
+	    datav.setUint32(14, this.bitmap.infoheader.biSize, true);
+	    datav.setUint32(18, this.bitmap.infoheader.biWidth, true);
+	    datav.setUint32(22, this.bitmap.infoheader.biHeight, true);
+	    datav.setUint16(26, this.bitmap.infoheader.biPlanes, true);
+	    datav.setUint16(28, this.bitmap.infoheader.biBitCount, true);
+	    datav.setUint32(30, this.bitmap.infoheader.biCompression, true);
+	    datav.setUint32(34, this.bitmap.infoheader.biSizeImage, true);
+	    datav.setUint32(38, this.bitmap.infoheader.biXPelsPerMeter, true);
+	    datav.setUint32(42, this.bitmap.infoheader.biYPelsPerMeter, true);
+	    datav.setUint32(46, this.bitmap.infoheader.biClrUsed, true);
+	    datav.setUint32(50, this.bitmap.infoheader.biClrImportant, true);
+      
+      // create blob from buffer and pixel array
+      var blop = new Blob([datav.buffer, this.bitmap.pixels], {type: "application/octet-stream"});
+      // save the file using FileSaver.min.js (github: https://github.com/eligrey/FileSaver.js/)
+      saveAs(blop, filename);
   	},
 
+    // render bitmap to given canvas
   	render:function (canvas) {
   		if (this.imageData != null)
   		{
@@ -106,54 +155,58 @@ BMP.prototype = {
   		}
   	},
 
+    // reads bitmap data from buffer
   	storeBitmap:function (buffer) {
+      // create DataView for buffer
   		var datav = new DataView(buffer); 
 
   		// file header
-		this.bitmap.fileheader = {}; 
-		this.bitmap.fileheader.bfType = 
-			datav.getUint16(0, true);
-		this.bitmap.fileheader.bfSize =
-            datav.getUint32(2, true);
-		this.bitmap.fileheader.bfReserved1 =
-            datav.getUint16(6, true);
-		this.bitmap.fileheader.bfReserved2 =
-            datav.getUint16(8, true);
-		this.bitmap.fileheader.bfOffBits =
-            datav.getUint32(10, true);
+		  this.bitmap.fileheader = {}; 
+		  this.bitmap.fileheader.bfType = 
+		  	      datav.getUint16(0, true);
+		  this.bitmap.fileheader.bfSize =
+              datav.getUint32(2, true);
+		  this.bitmap.fileheader.bfReserved1 =
+              datav.getUint16(6, true);
+		  this.bitmap.fileheader.bfReserved2 =
+              datav.getUint16(8, true);
+		  this.bitmap.fileheader.bfOffBits =
+              datav.getUint32(10, true);
 
-        // info header
-        this.bitmap.infoheader = {};
-		this.bitmap.infoheader.biSize =
-            datav.getUint32(14, true);
-		this.bitmap.infoheader.biWidth =
-            datav.getUint32(18, true);
-		this.bitmap.infoheader.biHeight =
-            datav.getUint32(22, true);
-		this.bitmap.infoheader.biPlanes =
-            datav.getUint16(26, true);
-		this.bitmap.infoheader.biBitCount =
-            datav.getUint16(28, true);
-		this.bitmap.infoheader.biCompression =
-            datav.getUint32(30, true);
-		this.bitmap.infoheader.biSizeImage =
-            datav.getUint32(34, true);
-		this.bitmap.infoheader.biXPelsPerMeter =
-            datav.getUint32(38, true);
-		this.bitmap.infoheader.biYPelsPerMeter =
-            datav.getUint32(42, true);
-		this.bitmap.infoheader.biClrUsed =
-            datav.getUint32(46, true);
-		this.bitmap.infoheader.biClrImportant =
-            datav.getUint32(50, true);
+      // info header
+      this.bitmap.infoheader = {};
+		  this.bitmap.infoheader.biSize =
+              datav.getUint32(14, true);
+		  this.bitmap.infoheader.biWidth =
+              datav.getUint32(18, true);
+		  this.bitmap.infoheader.biHeight =
+              datav.getUint32(22, true);
+		  this.bitmap.infoheader.biPlanes =
+              datav.getUint16(26, true);
+		  this.bitmap.infoheader.biBitCount =
+              datav.getUint16(28, true);
+		  this.bitmap.infoheader.biCompression =
+              datav.getUint32(30, true);
+		  this.bitmap.infoheader.biSizeImage =
+              datav.getUint32(34, true);
+		  this.bitmap.infoheader.biXPelsPerMeter =
+              datav.getUint32(38, true);
+		  this.bitmap.infoheader.biYPelsPerMeter =
+              datav.getUint32(42, true);
+		  this.bitmap.infoheader.biClrUsed =
+              datav.getUint32(46, true);
+		  this.bitmap.infoheader.biClrImportant =
+              datav.getUint32(50, true);
 
-        var start = this.bitmap.fileheader.bfOffBits;  
-        this.bitmap.stride = Math.floor((this.bitmap.infoheader.biBitCount * this.bitmap.infoheader.biWidth + 31) / 32) * 4;
-		this.bitmap.pixels = new Uint8Array(buffer, start);
+      // calculate stride (because pixels are aligned to DWORD for faster reading)
+      this.bitmap.stride = Math.floor((this.bitmap.infoheader.biBitCount * this.bitmap.infoheader.biWidth + 31) / 32) * 4;
+      // read pixels to byte array
+		  this.bitmap.pixels = new Uint8Array(buffer, this.bitmap.fileheader.bfOffBits);
   	},
 
-  	// imageData is for canvas 
+  	// generate and store ImageData for canvas rendering
   	storeImageData: function () {
+      // create temporary canvas to generate ImageData
   		var canvas = document.createElement("canvas");
   		var ctx = canvas.getContext("2d");
   		var Width = this.bitmap.infoheader.biWidth;
@@ -168,6 +221,7 @@ BMP.prototype = {
 		  		var index1 = (x+Width*(Height-y))*4;
 		  		var index2 = x * Math.ceil(this.bitmap.infoheader.biBitCount / 8) + stride * y;
 
+          // store pixels based on bit depth
 		  		if (this.bitmap.infoheader.biBitCount == 8 || this.bitmap.infoheader.biBitCount == 1){
 		  			data[index1] = bmpdata[index2];
 			  		data[index1 + 1] = bmpdata[index2];
@@ -185,6 +239,7 @@ BMP.prototype = {
   		 	}
 		  }
 
+      // store pixels
 		  this.imageData.data = data;
   	},
 
--- script.js
+++ script.js
@@ -8,14 +8,13 @@ setInterval(update, 200);
 function handleBMPInput (e) {
 	var file = e.target.files[0];
 	var reader = new FileReader();  
- 	reader.addEventListener("load",
-                       processImage, false); 
+ 	reader.addEventListener("load", processImage, false); 
  	reader.readAsArrayBuffer(file);
 } 
 
 function processImage (e) {
-	var buffer = e.target.result;
-	bmp = new BMP(buffer);
+	var buffer = e.target.result; // get result from FileReader
+	bmp = new BMP(buffer); // see bmp.js for class implementation
 
 	var context = canvas.getContext('2d');
 	bmp.render(canvas);
@@ -23,15 +22,20 @@ function processImage (e) {
 
 function update () {
 	var context = canvas.getContext('2d');
+	// clear canvas
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	if (bmp != null){	
+
+	if (bmp != null){
+		// some styling for canvas
 		canvas.style.maxWidth = canvas.width + "px";
 		canvas.width = bmp.getWidth();
 		canvas.height = bmp.getHeight();
+		// render bitmap
 		bmp.render(canvas);
 	}
 }
 
+// vertical = true, false
 function flip (vertical) {
 	if (bmp != null)
 		if(vertical)
@@ -40,7 +44,14 @@ function flip (vertical) {
 			bmp.flip(Direction.HORIZONTAL);
 }
 
+// ccw (counter clockwise) = true, false
 function rotate (ccw) {
 	if (bmp != null)
 		bmp.rotate(ccw);
+}
+
+// saves bitmap
+function save (){
+    if (bmp != null)
+        bmp.save("output.bmp");
 }
